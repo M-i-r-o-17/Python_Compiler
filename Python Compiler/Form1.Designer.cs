f683682a43b96e3ca11f2460a2d272e9ae6104a7
Vladimir <69893445+M-i-r-o-17@users.noreply.github.com>
2020-09-28 20:10:22 +0900

    Add files via upload

---
+++ Python Compiler/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace Python_Compiler
+{
+    partial class RootForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RootForm));
+            this.NotificationsGroup = new System.Windows.Forms.GroupBox();
+            this.Status = new System.Windows.Forms.Label();
+            this.MainGroup = new System.Windows.Forms.FlowLayoutPanel();
+            this.OtherSettings = new System.Windows.Forms.GroupBox();
+            this.CompileOneFile = new System.Windows.Forms.CheckBox();
+            this.OnOffConsole = new System.Windows.Forms.CheckBox();
+            this.LoadIcan = new System.Windows.Forms.Button();
+            this.AddIcon = new System.Windows.Forms.CheckBox();
+            this.Compiler = new System.Windows.Forms.GroupBox();
+            this.CompileFile = new System.Windows.Forms.Button();
+            this.OpenFile = new System.Windows.Forms.Button();
+            this.PathToFile = new System.Windows.Forms.TextBox();
+            this.LanguagePanel = new System.Windows.Forms.GroupBox();
+            this.SaveSettings = new System.Windows.Forms.Button();
+            this.Language = new System.Windows.Forms.ComboBox();
+            this.ChangeLanguage = new System.Windows.Forms.Button();
+            this.OpenIcon = new System.Windows.Forms.OpenFileDialog();
+            this.OpenScript = new System.Windows.Forms.OpenFileDialog();
+            this.NotificationsGroup.SuspendLayout();
+            this.MainGroup.SuspendLayout();
+            this.OtherSettings.SuspendLayout();
+            this.Compiler.SuspendLayout();
+            this.LanguagePanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // NotificationsGroup
+            // 
+            this.NotificationsGroup.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.NotificationsGroup.Controls.Add(this.Status);
+            resources.ApplyResources(this.NotificationsGroup, "NotificationsGroup");
+            this.NotificationsGroup.Name = "NotificationsGroup";
+            this.NotificationsGroup.TabStop = false;
+            // 
+            // Status
+            // 
+            resources.ApplyResources(this.Status, "Status");
+            this.Status.ForeColor = System.Drawing.Color.Brown;
+            this.Status.Name = "Status";
+            // 
+            // MainGroup
+            // 
+            this.MainGroup.Controls.Add(this.NotificationsGroup);
+            this.MainGroup.Controls.Add(this.OtherSettings);
+            this.MainGroup.Controls.Add(this.Compiler);
+            this.MainGroup.Controls.Add(this.LanguagePanel);
+            resources.ApplyResources(this.MainGroup, "MainGroup");
+            this.MainGroup.Name = "MainGroup";
+            // 
+            // OtherSettings
+            // 
+            this.OtherSettings.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.OtherSettings.Controls.Add(this.CompileOneFile);
+            this.OtherSettings.Controls.Add(this.OnOffConsole);
+            this.OtherSettings.Controls.Add(this.LoadIcan);
+            this.OtherSettings.Controls.Add(this.AddIcon);
+            resources.ApplyResources(this.OtherSettings, "OtherSettings");
+            this.OtherSettings.Name = "OtherSettings";
+            this.OtherSettings.TabStop = false;
+            // 
+            // CompileOneFile
+            // 
+            resources.ApplyResources(this.CompileOneFile, "CompileOneFile");
+            this.CompileOneFile.Checked = true;
+            this.CompileOneFile.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.CompileOneFile.Name = "CompileOneFile";
+            this.CompileOneFile.UseVisualStyleBackColor = true;
+            this.CompileOneFile.CheckedChanged += new System.EventHandler(this.CompileOneFile_CheckedChanged);
+            // 
+            // OnOffConsole
+            // 
+            resources.ApplyResources(this.OnOffConsole, "OnOffConsole");
+            this.OnOffConsole.Name = "OnOffConsole";
+            this.OnOffConsole.UseVisualStyleBackColor = true;
+            this.OnOffConsole.CheckedChanged += new System.EventHandler(this.OnOffConsole_CheckedChanged);
+            // 
+            // LoadIcan
+            // 
+            resources.ApplyResources(this.LoadIcan, "LoadIcan");
+            this.LoadIcan.Name = "LoadIcan";
+            this.LoadIcan.UseVisualStyleBackColor = true;
+            this.LoadIcan.Click += new System.EventHandler(this.LoadIcan_Click);
+            // 
+            // AddIcon
+            // 
+            resources.ApplyResources(this.AddIcon, "AddIcon");
+            this.AddIcon.Name = "AddIcon";
+            this.AddIcon.UseVisualStyleBackColor = true;
+            this.AddIcon.CheckedChanged += new System.EventHandler(this.AddIcon_CheckedChanged);
+            // 
+            // Compiler
+            // 
+            this.Compiler.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.Compiler.Controls.Add(this.CompileFile);
+            this.Compiler.Controls.Add(this.OpenFile);
+            this.Compiler.Controls.Add(this.PathToFile);
+            resources.ApplyResources(this.Compiler, "Compiler");
+            this.Compiler.Name = "Compiler";
+            this.Compiler.TabStop = false;
+            // 
+            // CompileFile
+            // 
+            resources.ApplyResources(this.CompileFile, "CompileFile");
+            this.CompileFile.Name = "CompileFile";
+            this.CompileFile.UseVisualStyleBackColor = true;
+            this.CompileFile.Click += new System.EventHandler(this.CompileFile_Click);
+            // 
+            // OpenFile
+            // 
+            resources.ApplyResources(this.OpenFile, "OpenFile");
+            this.OpenFile.Name = "OpenFile";
+            this.OpenFile.UseVisualStyleBackColor = true;
+            this.OpenFile.Click += new System.EventHandler(this.OpenFile_Click);
+            // 
+            // PathToFile
+            // 
+            resources.ApplyResources(this.PathToFile, "PathToFile");
+            this.PathToFile.Name = "PathToFile";
+            // 
+            // LanguagePanel
+            // 
+            this.LanguagePanel.Controls.Add(this.SaveSettings);
+            this.LanguagePanel.Controls.Add(this.Language);
+            this.LanguagePanel.Controls.Add(this.ChangeLanguage);
+            resources.ApplyResources(this.LanguagePanel, "LanguagePanel");
+            this.LanguagePanel.Name = "LanguagePanel";
+            this.LanguagePanel.TabStop = false;
+            // 
+            // SaveSettings
+            // 
+            resources.ApplyResources(this.SaveSettings, "SaveSettings");
+            this.SaveSettings.Name = "SaveSettings";
+            this.SaveSettings.UseVisualStyleBackColor = true;
+            this.SaveSettings.Click += new System.EventHandler(this.SaveSettings_Click);
+            // 
+            // Language
+            // 
+            this.Language.FormattingEnabled = true;
+            resources.ApplyResources(this.Language, "Language");
+            this.Language.Name = "Language";
+            // 
+            // ChangeLanguage
+            // 
+            resources.ApplyResources(this.ChangeLanguage, "ChangeLanguage");
+            this.ChangeLanguage.Name = "ChangeLanguage";
+            this.ChangeLanguage.UseVisualStyleBackColor = true;
+            this.ChangeLanguage.Click += new System.EventHandler(this.ChangeLanguage_Click);
+            // 
+            // OpenIcon
+            // 
+            this.OpenIcon.FileName = "openFileDialog1";
+            // 
+            // OpenScript
+            // 
+            this.OpenScript.FileName = "openFileDialog1";
+            // 
+            // RootForm
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.Controls.Add(this.MainGroup);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "RootForm";
+            this.NotificationsGroup.ResumeLayout(false);
+            this.NotificationsGroup.PerformLayout();
+            this.MainGroup.ResumeLayout(false);
+            this.OtherSettings.ResumeLayout(false);
+            this.OtherSettings.PerformLayout();
+            this.Compiler.ResumeLayout(false);
+            this.Compiler.PerformLayout();
+            this.LanguagePanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox NotificationsGroup;
+        private System.Windows.Forms.FlowLayoutPanel MainGroup;
+        private System.Windows.Forms.GroupBox OtherSettings;
+        private System.Windows.Forms.CheckBox CompileOneFile;
+        private System.Windows.Forms.CheckBox OnOffConsole;
+        private System.Windows.Forms.Button LoadIcan;
+        private System.Windows.Forms.CheckBox AddIcon;
+        private System.Windows.Forms.OpenFileDialog OpenIcon;
+        private System.Windows.Forms.GroupBox Compiler;
+        private System.Windows.Forms.Button CompileFile;
+        private System.Windows.Forms.Button OpenFile;
+        private System.Windows.Forms.TextBox PathToFile;
+        private System.Windows.Forms.GroupBox LanguagePanel;
+        private System.Windows.Forms.ComboBox Language;
+        private System.Windows.Forms.Button ChangeLanguage;
+        private System.Windows.Forms.Button SaveSettings;
+        private System.Windows.Forms.Label Status;
+        private System.Windows.Forms.OpenFileDialog OpenScript;
+    }
+}
+
---
+++ Python Compiler/Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Python_Compiler
+{
+    public partial class RootForm : Form
+    {
+        public RootForm()
+        {
+            InitializeComponent();
+            Start();
+        }
+
+        private bool HavePycompiler = false;
+        private bool AddIconBox = false;
+        private bool EnableConsoleBox = false;
+        private bool CreateOneFileBox = true;
+
+        private string[,] LanguageApp =
+            {
+                {
+                    "English",
+                    "Notifications",
+                    "To correct work you need \"pyinstaller\"",
+                    "Other settings",
+                    "Add icon",
+                    "Open",
+                    "Enable console",
+                    "Compill in one file",
+                    "Compiler",
+                    "Open file",
+                    "Compile",
+                    "Language",
+                    "Change",
+                    "Save"
+                },
+                {
+                    "Русский",
+                    "Увидомления",
+                    "Для правельной работы нужен \"pyinstaller\"",
+                    "Прочие настройки",
+                    "Добавить иконку",
+                    "Открыть",
+                    "Включить консоль",
+                    "Скомпилировать в один фаил",
+                    "Компилятор",
+                    "Открыть фаил",
+                    "Скомпилировать",
+                    "Язык",
+                    "Сменить",
+                    "Сохранить"
+                }
+            };
+
+        private string[] Paths = { "", "" };
+        private bool StringToBool(string value)
+        {
+            value = value.ToLower();
+            if (value == "true")
+                return true;
+            else
+                return false;
+        }
+       
+        private void DefaultSettings()
+        {
+            Language.SelectedIndex = 0;
+            ChangeLanguageApp();
+
+            AddIconBox = false;
+            EnableConsoleBox = false;
+            CreateOneFileBox = false;
+        }
+        
+        private void ChangeLanguageApp(int IdLanguage = 0)
+        {
+
+            /*Настройка увидомлений*/
+            NotificationsGroup.Text = LanguageApp[IdLanguage, 1];
+            Status.Text = LanguageApp[IdLanguage, 2];
+            /*Прочие настройки*/
+            OtherSettings.Text = LanguageApp[IdLanguage, 3];
+            AddIcon.Text = LanguageApp[IdLanguage, 4];
+            LoadIcan.Text = LanguageApp[IdLanguage, 5];
+            OnOffConsole.Text = LanguageApp[IdLanguage, 6];
+            CompileOneFile.Text = LanguageApp[IdLanguage, 7];
+
+            /*Компилятор*/
+            Compiler.Text = LanguageApp[IdLanguage, 8];
+            OpenFile.Text = LanguageApp[IdLanguage, 9];
+            CompileFile.Text = LanguageApp[IdLanguage, 10];
+
+            /*Язык*/
+            LanguagePanel.Text = LanguageApp[IdLanguage, 11];
+            ChangeLanguage.Text = LanguageApp[IdLanguage, 12];
+            SaveSettings.Text = LanguageApp[IdLanguage, 13];
+        }
+        private void ChangeVariabels(string name, string value)
+        {
+            switch (name)
+            {
+                case "Language":
+                    Language.SelectedIndex = int.Parse(value);
+                    ChangeLanguageApp(int.Parse(value));
+                    break;
+                case "Add Icon":
+                    AddIconBox = AddIcon.Checked = StringToBool(value);
+                    break;
+                case "Enabel console":
+                    EnableConsoleBox = OnOffConsole.Checked = StringToBool(value);
+                    break;
+                case "Compile one file":
+                    CreateOneFileBox = CompileOneFile.Checked = StringToBool(value);
+                    break;
+
+            }
+
+        }
+        
+        private void SaveSettingsApp()
+        {
+            using (StreamWriter sw = new StreamWriter("Settings.cfg", false, System.Text.Encoding.Default))
+            {
+                sw.WriteLine($"Language:{Language.SelectedIndex}");
+                sw.WriteLine($"Add Icon:{AddIconBox}");
+                sw.WriteLine($"Enabel console:{EnableConsoleBox}");
+                sw.Write($"Compile one file:{CreateOneFileBox}");
+            }
+        }
+        private bool LoadSettingsApp()
+        {
+            if (File.Exists("Settings.cfg"))
+            {
+                using(StreamReader sr = new StreamReader("Settings.cfg"))
+                {
+
+                    string LineSettings = "";
+                    while((LineSettings = sr.ReadLine()) != null)
+                    {
+                        string[] command = LineSettings.Split(':');
+                        ChangeVariabels(command[0], command[1]);
+                    }
+                }
+                return true;
+            }
+            else
+                return false;
+        }
+        
+
+        private void Start()
+        {
+            for(byte i = 0; i < LanguageApp.GetLength(0); i++)
+            {
+                Language.Items.Add(LanguageApp[i, 0]);
+            }
+            if (LoadSettingsApp() == false) { DefaultSettings(); SaveSettingsApp(); };
+        }
+
+        /*Нажатия кнопок*/
+        private void ChangeLanguage_Click(object sender, EventArgs e)
+        {
+            int id = Language.SelectedIndex;
+            ChangeLanguageApp(id);
+        }
+
+        private void SaveSettings_Click(object sender, EventArgs e)
+        {
+            SaveSettingsApp();
+        }
+
+        private void AddIcon_CheckedChanged(object sender, EventArgs e)
+        {
+            AddIconBox = !AddIconBox;
+            LoadIcan.Enabled = AddIconBox;
+        }
+
+        private void OnOffConsole_CheckedChanged(object sender, EventArgs e)
+        {
+            EnableConsoleBox = !EnableConsoleBox;
+        }
+
+        private void CompileOneFile_CheckedChanged(object sender, EventArgs e)
+        {
+            CreateOneFileBox = !CreateOneFileBox;
+        }
+
+        private void LoadIcan_Click(object sender, EventArgs e)
+        {
+            OpenIcon.Filter = "Image Files(*.BMP;*.JPG;*PNG)|*.BMP;*.JPG;*PNG";
+            OpenIcon.FileName = String.Empty;
+            OpenIcon.ShowDialog();
+            Paths[1] = OpenIcon.FileName;
+        }
+
+        private void OpenFile_Click(object sender, EventArgs e)
+        {
+            OpenScript.Filter = "Python file|*.py";
+            OpenScript.FileName = String.Empty;
+            OpenScript.ShowDialog();
+            Paths[0] = OpenScript.FileName;
+            PathToFile.Text = Paths[0];
+        }
+
+        private void CompileFile_Click(object sender, EventArgs e)
+        {
+            string command = "pyinstaller ";
+            if (!EnableConsoleBox)
+                command += "-w ";
+            if (CreateOneFileBox)
+                command += "-F ";
+            if (AddIconBox && Paths[1] != "")
+                command += $"-i \"{Paths[1]}\"";
+            command += Paths[0];
+            if (Paths[0] != "")
+            {
+                System.Diagnostics.Process.Start("CMD.exe", "/C " + command);
+                System.Diagnostics.Process.Start("CMD.exe", "/C explorer dist");
+            }
+        }
+    }
+}
